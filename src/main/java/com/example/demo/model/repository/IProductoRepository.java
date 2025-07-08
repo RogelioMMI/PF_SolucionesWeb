986864946326dc4fde46aadac0b3a36c49338023
RogelioMMI <rogelio_c.r@hotmail.com>
2025-07-08 16:59:51 -0500

    Update IProductoRepository.java

--- src/main/java/com/example/demo/model/repository/IProductoRepository.java
+++ src/main/java/com/example/demo/model/repository/IProductoRepository.java
@@ -1,9 +1,13 @@
 package com.example.demo.model.repository;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.example.demo.model.entity.Producto;
 
 public interface IProductoRepository extends CrudRepository<Producto, Long>{
-
+    // IProductoRepository.java
+List<Producto> findByCategoriaIgnoreCase(String categoria);
+    
 }

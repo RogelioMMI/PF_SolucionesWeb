4456635fd2fb59d90f97acbc90cf273ae81c56d1
RogelioMMI <rogelio_c.r@hotmail.com>
2025-07-08 21:43:54 -0500

    Update IPedidoRepository.java

--- src/main/java/com/example/demo/model/repository/IPedidoRepository.java
+++ src/main/java/com/example/demo/model/repository/IPedidoRepository.java
@@ -1,9 +1,12 @@
 package com.example.demo.model.repository;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
+import com.example.demo.model.entity.Cliente;
 import com.example.demo.model.entity.Pedido;
 
 public interface IPedidoRepository extends CrudRepository<Pedido, Long>{
-
+    List<Pedido> findByCliente(Cliente cliente);
 }

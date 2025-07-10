848dce5b95973a02210682ceac70d31c002202c5
RogelioMMI <rogelio_c.r@hotmail.com>
2025-07-10 08:13:50 -0500

    Update DetallePedidoService.java

--- src/main/java/com/example/demo/model/service/DetallePedidoService.java
+++ src/main/java/com/example/demo/model/service/DetallePedidoService.java
@@ -38,17 +38,6 @@ public class DetallePedidoService implements IDetallePedidoService {
         return detallePedidoRepository.findById(id).orElse(null);
     }
 
-    // @Override
-    // public String eliminarDetallePedido(Long id) {
-    //     DetallePedido detalle = detallePedidoRepository.findById(id).orElse(null);
-    //     if (detalle == null) return "Detalle no encontrado";
-
-    //     Pedido pedido = detalle.getPedido();
-    //     detallePedidoRepository.deleteById(id);
-
-    //     actualizarTotalPedido(detalle);
-    //     return "Se eliminó el detalle de pedido";
-    // }
     @Override
     public String eliminarDetallePedido(Long id) {
         DetallePedido detalle = detallePedidoRepository.findById(id).orElse(null);
